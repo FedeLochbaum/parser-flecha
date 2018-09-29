276577b7a388fc8fb2684fa2a3e2f4ddbe266b49
trimegisto <federico.lochbaum@gmail.com>
2018-09-29 17:04:32 -0300

    advance in parser

--- src/main/scala/FlechaParser.scala
+++ src/main/scala/FlechaParser.scala
@@ -1,10 +1,26 @@
 import play.api.libs.json.Json
+import tokens.{DEFToken, EOFToken, Token}
 
 case class FlechaParser(input : String) {
   val lexer = FlechaLexer(input.iterator.buffered)
+  advanceToken
 
-  def currentToken = lexer.nextToken
+  var currentToken : Token
+  def advanceToken = { currentToken = lexer.nextToken }
+
   def parse = {
-    Json.parse("")
+    var definitions = List()
+    while (currentToken != EOFToken()) { definitions.+:(parseDefinition) }
+    ProgramAST(definitions)
   }
+
+  def parseDefinition = {
+    currentToken match {
+      case DEFToken() => parseDEF
+      case  _         => error("Definition")
+    }
+  }
+  def error(expectedType: String) = throw new FlechaParseError(expectedType, currentToken)
 }
+
+class FlechaParseError(val expectedType: String, token: Token) extends Exception(s"Parse error: ExpectedType: $expectedType but found ${token.toString}")
