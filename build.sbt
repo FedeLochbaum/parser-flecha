d9d67529b7ae3e2198deb5e767e43f9e7c75c911
trimegisto <federico.lochbaum@gmail.com>
2018-10-04 22:38:57 -0300

    add prettier for json ast

--- build.sbt
+++ build.sbt
@@ -5,6 +5,7 @@ version := "0.1"
 scalaVersion := "2.12.6"
 
 libraryDependencies += "com.typesafe.play" %% "play-json" % "2.6.10"
+libraryDependencies += "com.lihaoyi" %% "upickle" % "0.6.6"
 libraryDependencies += "org.scalactic" %% "scalactic" % "3.0.5"
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.5" % "test"
 resolvers += "Artima Maven Repository" at "http://repo.artima.com/releases"
--- src/main/scala/FlechaParserApp.scala
+++ src/main/scala/FlechaParserApp.scala
@@ -3,13 +3,14 @@ import java.io.{BufferedWriter, File, FileWriter}
 import scala.io.Source
 
 object Parser extends App {
-  def parse(fileName: String) = {
+  def parse(fileName: String, indent: Int) = {
     val input = Source.fromFile(fileName).mkString
     val res = FlechaParser(input).parse
+    val show = ujson.reformat(res.toString(), indent = indent)
     val file = new File(s"$fileName.AST")
     val bw = new BufferedWriter(new FileWriter(file))
-    bw.write(res.toString)
+    bw.write(show)
     bw.close()
-    res
+    show
   }
 }
